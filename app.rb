cb5120fdb72c4420572deda1f88f0afc50953f05
eaglesant <eagle_21@mail.ru>
2017-04-04 02:03:11 +0300

    Crete table Posts

--- app.rb
+++ app.rb
@@ -5,21 +5,31 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def init_db
-    @db = SQLite3::Database.new 'leproblog.db'
-    @db.results_as_hash
+	@db = SQLite3::Database.new 'leproblog.db'
+	@db.results_as_hash
 end
 before do
-    init_db
+	init_db
+end
+
+configure do
+	init_db
+	@db.execute 'CREATE TABLE if not exists Posts
+	(
+	    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+	    created_date DATE,
+	    content      TEXT
+	)'
 end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 get '/new' do
-  erb :new
+	erb :new
 end
 post '/new' do
-  @post = params[:content]
+	@post = params[:content]
 
-  erb "You typed #{@post}"
+	erb "You typed #{@post}"
 end
